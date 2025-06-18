bf6a511e54b97a9545ffc0843d0333305a265c02
heitorl <juniorio01@hotmail.com>
2025-06-17 22:47:52 -0300

    refactor logic fetch to action file

--- app/_actions/student.action.ts
+++ app/_actions/student.action.ts
@@ -1,15 +1,42 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { importFileXlsx } from "../_services/student.service";
+
+const apiUrl = "https://api-cert-c9xz.onrender.com/students";
 
 export const uploadFileAction = async (file: File) => {
   try {
-    const result = await importFileXlsx(file);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch(`${apiUrl}/upload`, {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data?.error || "Erro ao importar planilha");
+    }
 
     revalidatePath(`/`);
-    return result;
+    return data;
   } catch (error: any) {
     throw new Error(error);
+  }
+};
+
+export const getAllStudentsData = async (): Promise<StudentType[]> => {
+  try {
+    const response = await fetch(`${apiUrl}`);
+    console.log(response);
+    if (!response.ok) {
+      throw new Error("Erro ao certificados");
+    }
+
+    return await response.json();
+  } catch (error: any) {
+    throw error;
   }
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-import SubHeader, {
-  SubHeaderLeft,
-  SubHeaderRight,
-  SubHeaderTitle,
-} from "./_components/sub-header";
+import SubHeader from "./_components/sub-header";
 import { TableCertificate } from "./_components/table-certificate";
-import { Button } from "./_components/ui/button";
-import { Download } from "lucide-react";
-import { getAllStudentsData } from "./_services/student.service";
-import { ButtonAddFile } from "./_components/button-add-file";
 import { ButtonsMenu } from "./_components/buttons-menu";
+import { getAllStudentsData } from "./_actions/student.action";
 
 export default async function Home() {
   const students = await getAllStudentsData();
