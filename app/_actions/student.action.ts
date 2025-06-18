e690f949dc968ed7887c2cacc338a74dc9cb4cf0
heitorl <juniorio01@hotmail.com>
2025-06-18 16:21:01 -0300

    change fetch next to axios

--- app/_actions/student.action.ts
+++ app/_actions/student.action.ts
@@ -1,42 +1,38 @@
 "use server";
 
+import axios from "axios";
 import { revalidatePath } from "next/cache";
 
 const apiUrl = "https://api-cert-c9xz.onrender.com/students";
+// const apiUrl = "http://localhost:3333/students";
 
 export const uploadFileAction = async (file: File) => {
   try {
     const formData = new FormData();
     formData.append("file", file);
 
-    const response = await fetch(`${apiUrl}/upload`, {
-      method: "POST",
-      body: formData,
+    const response = await axios.post(`${apiUrl}/upload`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
     });
 
-    const data = await response.json();
-
-    if (!response.ok) {
-      throw new Error(data?.error || "Erro ao importar planilha");
-    }
-
     revalidatePath(`/`);
-    return data;
+    return response.data;
   } catch (error: any) {
-    throw new Error(error);
+    const message =
+      error?.response?.data?.error || "Erro ao buscar certificados";
+    throw new Error(message);
   }
 };
 
 export const getAllStudentsData = async (): Promise<StudentType[]> => {
   try {
-    const response = await fetch(`${apiUrl}`);
-    console.log(response);
-    if (!response.ok) {
-      throw new Error("Erro ao certificados");
-    }
-
-    return await response.json();
+    const response = await axios.get<StudentType[]>(`${apiUrl}`);
+    return response.data;
   } catch (error: any) {
-    throw error;
+    const message =
+      error?.response?.data?.error || "Erro ao buscar certificados";
+    throw new Error(message);
   }
 };
